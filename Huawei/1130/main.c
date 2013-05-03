7bccae48b92d6e49245afbd6d1ea5bc7bd95912e
shmily <shmily@mail.dlut.edu.cn>
2013-05-03 23:42:34 +0800

    finish the 1130

--- Huawei/1130/main.c
+++ Huawei/1130/main.c
@@ -18,16 +18,31 @@ int mahjong[3][10]; // T,D,W
 
 void paserInput(char *str);
 void print_arry(void);
+int analyze(int *arry);
+int isWin(void);
+int is7pair(void);
 
 int main(void)
 {
 	char str[32];
+    int  flag;
 
 	memset(mahjong, 0, sizeof(int)*2*10);
+	print_arry();
 	gets(str);
 	paserInput(str);
 
 	print_arry();
+    flag = isWin();
+
+    if(flag==-1){
+        flag = is7pair();
+    }
+
+    if(flag==0)
+        printf("Hu!\n");
+    else 
+        printf("Not Hu!\n");
 
 	return 0;
 }
@@ -56,20 +71,24 @@ int isWin(void)
 	int i, pair, nopair;
 	int ret;
 
-	if((mahjong[0][0]=!0)&&(mahjong[1][0]!=0)&&(mahjong[2][0]!=0)){
-		return -2;
+	print_arry();
+	if((mahjong[0][0]!=0)&&(mahjong[1][0]!=0)&&(mahjong[2][0]!=0)){
+		printf("the plate is over...\n");
+        return -2;
 	}
 
 	pair   = -1;
 	nopair = -1;
 	for(i=0; i<3; i++){
 		if( mahjong[i][0]%3==1 ){
+		    printf("the plate is . 1...%d,%d\n", mahjong[i][0],i);
 			return -2;
 		}else if(mahjong[i][0]%3==2){	// 3n+2
 			
 			if(pair==-1){
 				pair = i;
 			}else{						// Impossible have 2 (3n+2)
+		        printf("the plate is 2 2...\n");
 				return -2;
 			}
 		}else{	// 3n
@@ -84,8 +103,39 @@ int isWin(void)
 	}
 
 	if(pair!=-1){
-		for(i=0; i<10; i++)
+		for(i=1; i<10; i++){
+            if(mahjong[pair][i]>=2){
+                mahjong[pair][i] -= 2;
+                mahjong[pair][0] -= 2;
+                ret = analyze(mahjong[pair]);
+                mahjong[pair][i] += 2;
+                mahjong[pair][0] += 2;
+
+                if(ret==0) break;
+            }
+        }
 	}
+
+    return ret;
+}
+
+int is7pair(void)
+{
+    int i,j;
+
+    printf("7 pair detect...\n");
+
+    for(i=0; i<3; i++){
+        if(mahjong[i][0]!=0){
+            for(j=1; j<10; j++){
+                if(mahjong[i][j]%2!=0){
+                    return -2;
+                }
+            }
+        }
+    }
+
+    return 0;
 }
 
 int analyze(int *arry)
